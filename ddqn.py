1a10da93316e231253f6bf673c0ab87ead50a865
chong.xie <chong.xie@bytedance.com>
2023-04-04 10:36:45 +0800

    Upload ddqn code

--- ddqn.py
+++ ddqn.py
@@ -12,32 +12,38 @@ from tensorflow.keras.losses import mean_squared_error
 ENV_NAME = "CartPole-v1"
 
 GAMMA = 0.95
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.01
 
-MEMORY_SIZE = 1000000
-BATCH_SIZE = 20
+MEMORY_SIZE = 10000
+BATCH_SIZE = 64
 
 EXPLORATION_MAX = 1.0
-EXPLORATION_MIN = 0.01
+EXPLORATION_MIN = 0.001
 EXPLORATION_DECAY = 0.995
 
-
-
+# How often to update the target network
+UPDATE_TARGET_FREQUENCY = 10
 optimizer=Adam(lr=LEARNING_RATE)
 
-class DQNSolver:
+class DDQNSolver:
 
     def __init__(self, observation_space, action_space):
         self.exploration_rate = EXPLORATION_MAX
 
         self.action_space = action_space
+        self.observation_space = observation_space
         self.memory = deque(maxlen=MEMORY_SIZE)
 
-        self.model = Sequential()
-        self.model.add(Dense(24, input_shape=(observation_space,), activation="relu"))
-        self.model.add(Dense(24, activation="relu"))
-        self.model.add(Dense(self.action_space, activation="linear"))
+        self.model = self.create_q_network()
+        self.model_target = self.create_q_network()
 
+    
+    def create_q_network(self):
+        model = Sequential()
+        model.add(Dense(24, input_shape=(self.observation_space,), activation="relu"))
+        model.add(Dense(24, activation="relu"))
+        model.add(Dense(self.action_space, activation="linear"))
+        return model
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
@@ -62,27 +68,28 @@ class DQNSolver:
         state_next_sample  = np.array([self.memory[i][3] for i in indices])
         done_sample = np.array([self.memory[i][4] for i in indices])
 
-        future_rewards = self.model.predict(state_next_sample).squeeze()
-        # Q value = reward + discount factor * expected future reward
-        updated_q_values = rewards_sample + GAMMA * tf.reduce_max(future_rewards, axis=1)
-        # print(f'updated_q_values: {updated_q_values}')
 
-        # If final frame set the last value to -1
-        updated_q_values = updated_q_values * (1 - done_sample) - done_sample
-
+        current_q_batch = self.model.predict(state_next_sample).squeeze()
+        max_action_next = tf.argmax(current_q_batch, axis=1)
+        max_action_next = tf.reshape(max_action_next, shape=(-1, 1))
         # Create a mask so we only calculate loss on the updated Q-values
-        masks = tf.one_hot(action_sample, self.action_space)
+        target_q_batch = self.model_target.predict(state_next_sample).squeeze()
+        # Q value = reward + discount factor * expected future reward
+        target_q_batch = rewards_sample + GAMMA * tf.squeeze(tf.gather_nd(target_q_batch, max_action_next, batch_dims=1))
+        # If final frame set the last value to -1
+        target_q_batch = target_q_batch * (1 - done_sample) - done_sample
 
         with tf.GradientTape() as tape:
             # Train the model on the states and updated Q-values
             q_values = self.model(state_sample)
-            
             q_values = tf.squeeze(q_values, [1])
+
             # Apply the masks to the Q-values to get the Q-value for action taken
+            masks = tf.one_hot(action_sample, self.action_space)
             q_action = tf.reduce_max((tf.multiply(q_values, masks)), axis=1)
             
-            # # Calculate loss between new Q-value and old Q-value
-            loss = mean_squared_error(updated_q_values, q_action)
+            # Calculate loss between new Q-value and old Q-value
+            loss = mean_squared_error(target_q_batch, q_action)
 
             # Backpropagation
             grads = tape.gradient(loss, self.model.trainable_variables)
@@ -95,7 +102,7 @@ def cartpole():
     env = gym.make(ENV_NAME, render_mode="rgb_array")
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.n
-    dqn_solver = DQNSolver(observation_space, action_space)
+    dqn_solver = DDQNSolver(observation_space, action_space)
     run = 0
     while True:
         run += 1
@@ -115,6 +122,12 @@ def cartpole():
             if terminated:
                 print("Run: " + str(run) + ", exploration: " + str(dqn_solver.exploration_rate) + ", score: " + str(step))
                 break
-            dqn_solver.experience_replay()
 
+            dqn_solver.experience_replay()
+        
+        # Update the the target network with new weights
+        if run % UPDATE_TARGET_FREQUENCY == 0:
+            dqn_solver.model_target.set_weights(dqn_solver.model.get_weights())
+        
+        
 cartpole()
